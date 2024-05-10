91a36466312b7e14fbb6a91fe67fa3d81f8a2cc7
Pathik Patel <patelpathik1995@gmail.com>
2024-05-11 04:41:28 +0530

    initial project commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response } from 'express'
+
+import { configDotenv } from 'dotenv'
+
+configDotenv()
+
+const app: Express = express()
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: Request, res: Response) => {
+  res.send({ msg: 'Express + TypeScript Server' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT} ${new Date().getTime()}`)
+})
